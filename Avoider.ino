4b0f15be66e5b341ae92b81e9231eca5f094e8ed
Mario Octavio Jimenez <jimenez303@gmail.com>
2017-01-07 12:25:50 -0600

    added support for accelerometer

--- Avoider.ino
+++ Avoider.ino
@@ -1,5 +1,6 @@
 #include <Servo.h>
 
+// Ultrasonic sensor
 #define ECHO_PIN 4
 #define TRIGGER_PIN 5
 #define SERVO_PIN 6
@@ -8,16 +9,23 @@
 #define ACTION_DIST 25
 #define MIN_ACTION_DIST 10
 
-#define SPEED 150
+#define SPEED_A 150
+#define SPEED_B 125
 #define CENTER_POS 80 // usually 90deg, set in 80 to adjust the physical servo position
-#define TURN_DELAY 500
+#define TURN_DELAY 350
 #define SERVO_SPEED 0.1/60 // SG90 TowerPro angular speed
+
+// Accelerometer
+const int groundpin = 18;             // analog input pin 4 -- ground
+const int zpin = A1;                  // z-axis
 
 Servo servo; // create servo object to control a Servo
 
 unsigned long dist_fwd, left_dist, right_dist; // Distance measured
 
 void setup() {
+  pinMode(groundpin, OUTPUT);
+  digitalWrite(groundpin, LOW);  
   servo.attach(SERVO_PIN);
   servo.write(CENTER_POS);
 
@@ -39,7 +47,7 @@ void setup() {
   delay(4000);
 }
 
-void loop() {
+void loop() {    
   // Get a reading from the current sensor direction
   dist_fwd = get_distance();
 
@@ -50,7 +58,6 @@ void loop() {
     if (dist_fwd <= MIN_ACTION_DIST) {
       go_backward();
     } else {
-      halt();
       find_path();
     }
   }
@@ -58,6 +65,7 @@ void loop() {
 
 // Measure distance find a new path
 void find_path() {
+  halt();
   for (int pos = CENTER_POS; pos >= 0; pos -= 1) {
     servo.write(pos);
     delay(1000 * SERVO_SPEED);
@@ -124,7 +132,7 @@ void go_left() {
   // Motor A forward
   digitalWrite(12, HIGH); // Establishes forward direction of Channel A
   digitalWrite(9, LOW); // Disengage the Brake for Channel A
-  analogWrite(3, SPEED); // Spins the motor on Channel A at full speed
+  analogWrite(3, SPEED_A); // Spins the motor on Channel A at full speed
 
   delay(TURN_DELAY);
   halt();
@@ -137,7 +145,7 @@ void go_right() {
   // Motor B forward
   digitalWrite(13, HIGH); // Establishes forward direction of Channel A
   digitalWrite(8, LOW); // Disengage the Brake for Channel A
-  analogWrite(11, SPEED); // Spins the motor on Channel A at full speed
+  analogWrite(11, SPEED_B); // Spins the motor on Channel A at full speed
 
   delay(TURN_DELAY);
   halt();
@@ -150,12 +158,27 @@ void go_forward() {
   // Motor A forward
   digitalWrite(12, HIGH); // Establishes forward direction of Channel A
   digitalWrite(9, LOW); // Disengage the Brake for Channel A
-  analogWrite(3, SPEED); // Spins the motor on Channel A at full speed
+  analogWrite(3, SPEED_A); // Spins the motor on Channel A at full speed
 
   //   Motor B forward
   digitalWrite(13, HIGH); // Establishes forward direction of Channel A
   digitalWrite(8, LOW); // Disengage the Brake for Channel A
-  analogWrite(11, SPEED); // Spins the motor on Channel A at full speed
+  analogWrite(11, SPEED_B); // Spins the motor on Channel A at full speed
+
+  delay(TURN_DELAY);
+  if(!is_accelerating()){       
+    go_backward();  
+    delay(TURN_DELAY);
+    find_path();
+  }
+}
+
+boolean is_accelerating(){  
+  int z_axis = analogRead(zpin);
+  delay(100);
+  Serial.println(z_axis);
+  
+  return z_axis > 18 ? true : false;
 }
 
 void go_backward() {
@@ -165,10 +188,11 @@ void go_backward() {
   // Motor A backward
   digitalWrite(12, LOW); // Establishes backward direction of Channel A
   digitalWrite(9, LOW); // Disengage the Brake for Channel A
-  analogWrite(3, SPEED); // Spins the motor on Channel A at half speed
+  analogWrite(3, SPEED_A); // Spins the motor on Channel A at half speed
 
   // Motor B backward
   digitalWrite(13, LOW); // Establishes backward direction of Channel B
   digitalWrite(8, LOW); // Disengage the Brake for Channel B
-  analogWrite(11, SPEED); // Spins the motor on Channel B at half speed
+  analogWrite(11, SPEED_B); // Spins the motor on Channel B at half speed
 }
+
